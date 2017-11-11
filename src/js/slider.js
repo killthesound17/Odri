bfd56a7f74be9f3954877923d81bf9b466775871
amCriminal <killthesound17@gmail.com>
2017-11-11 19:08:48 +0200

    Slider fixed
    slider.js added

---
+++ src/js/slider.js
@@ -0,0 +1,34 @@
+$('.photos-fullsize').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+    autoplaySpeed: 2000,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
