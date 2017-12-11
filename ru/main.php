0f80a7b895c8a9c29bc3613a78c5bf45742aeb08
amCriminal <killthesound17@gmail.com>
2017-12-11 04:17:17 +0200

    New year main page added

---
+++ ru/main.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400|Rochester|Roboto:300,400" rel="stylesheet">
+  <link rel="stylesheet" href="../src/css/normalize.css">
+  <link rel="stylesheet" href="../src/css/main.css">
+  <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+  <script type="text/javascript" src="../src/js/jquery-3.2.1.min.js">
+  </script>
+
+  <title>ODRI Model Management - Модельное агентство</title>
+</head>
+<body>
+  <?php include 'preloader.php' ?>
+  <div class="container">
+    <div class="lang-buttons">
+      <a href="../en/main.php">EN</a>
+      <a href="#">RU</a>
+    </div>
+  </div>
+  <header>
+      <?php
+      include 'ru-header.html' ?>
+  </header>
+
+
+  <main>
+<section class="main-odri">
+  <div class="snow"></div>
+  <div class="container">
+    <div class="wrapper">
+    <div class="main-info">
+      <h3 class="main-info__heading">Супер предложение от</h3>
+      <h2 class="main-info__odri-heading">"Odri Models"!</h2>
+      <p class="main-info__text">Зачем дарить звезду с неба, если можно самому стать ею!</p>
+      <p class="main-info__text">Подарите Вашему ребёнку обучение в нашем модельном агентстве.</p>
+      <a href="../ru/model-school.php" class="main-info__link">Подробнее о школе &rarr;</a><br>
+      <a href="../ru/contacts.php" class="main-info__link">Связаться с нами&rarr;</a>
+
+
+    </div>
+    </div>
+  </div>
+</section>
+
+  </main>
+  <footer>
+    <?php include 'ru-footer.php' ?>
+  </footer>
+    <script type="text/javascript" src="../src/js/preloader.js"></script>
+    <script src="https://use.fontawesome.com/e059081491.js"></script>
+    <script type="text/javascript" src="../src/js/main.js"></script>
+</body>
+</html>
