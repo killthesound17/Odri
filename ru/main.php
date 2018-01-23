f984f4985c0273d74af62ceb180f19beb8388d7f
amCriminal <killthesound17@gmail.com>
2018-01-23 03:33:36 +0200

    contest page added

--- ru/main.php
+++ ru/main.php
@@ -34,7 +34,7 @@
   <div class="snow"></div>
   <div class="container">
     <div class="wrapper">
-    <div class="main-info">
+    <!-- <div class="main-info">
       <h3 class="main-info__heading">Супер предложение от</h3>
       <h2 class="main-info__odri-heading">"Odri Models"!</h2>
       <p class="main-info__text">Зачем дарить звезду с неба, если можно самому стать ею!</p>
@@ -43,7 +43,37 @@
       <a href="../ru/contacts.php" class="main-info__link">Связаться с нами&rarr;</a>
 
 
+    </div> -->
+    <div class="odri-contest">
+      <div class="contest-info">
+        <div class="contest-textinfo">
+          <h1 class="contest-textinfo__heading">
+          Конкурс от Odri Models
+          </h1>
+          <p class="contest-textinfo__desc">Рады сообщить, что Odri Models Management проводит конкурс "Mini Model of the Year" и объявляет набор участниц! </p>
+          <p class="contest-textinfo__desc">Ежемесячно по итогам свободного голосования будут выбираться Mini Model месяца, которые в конце года смогут посоревноваться за главный приз - корону и титул "Mini Model of the Year"!</p>
+          <p class="contest-textinfo__desc">Подробнее об участии в конкурсе: <a href="" class="contest-textinfo__link">связаться с нами</a></p>
+          <p class="contest-textinfo__desc">Поддержите наших юных красавиц в борьбе за звание "Mini Model of January".</p>
+        </div>
+        <div class="contest-photo">
+          <img src="../src/img/contest/modelOfJanuary.png" alt="" class="contest-photo__crownimg">
+        </div>
+
+      </div>
+      <div class="minimiss-photos">
+        <div class="minimiss-photos__section">
+         <img src="../src/img/contest/dudchenko.jpg" alt="" class="minimiss-photos__img"></div>
+        <div class="minimiss-photos__section">
+         <img src="../src/img/contest/skakun.jpg" alt="" class="minimiss-photos__img">    </div>
+        <div class="minimiss-photos__section">
+         <img src="../src/img/contest/skorobogach.jpg" alt="" class="minimiss-photos__img">
+         </div>
+      </div>
+      <div class="polesection"><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeTbqUkWY2qZT0RMunHwW8OZODTxVv6oBsItQ-b9v1howcMjw/viewform?embedded=true" width="500" height="750" frameborder="0" marginheight="0" marginwidth="0">Загрузка...</iframe></div>
     </div>
+
+
+
     </div>
   </div>
 </section>
